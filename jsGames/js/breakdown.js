14675a68a0d2aeced8f082767de64e26d16e159b
t.koudu <t.koudu@netlinks.co.jp>
2015-10-07 00:45:53 +0900

    更新

--- jsGames/js/breakdown.js
+++ jsGames/js/breakdown.js
@@ -4,22 +4,27 @@ var count = 0;
 // プレイヤー定義　定数にする方法がわからない
 var player1 = "player1";
 var player2 = "player2";
+var nowPlayer = player1;
 
+var clickCount = 0 ;
+var selectImageId = -1;
 var panelImages = new Object() ;
 
+/**
+* テーブル要素を作成する
+*/
 function creatPanel(count)
 {
-
+  // スタートボタンを隠す
   changeTypeHidden($('#startButton'));
 
   var table = $('table');
   var panelCount = 0 ;
-  for(var i = 1 ; i <= count ; i++){
+  for(var i = 0 ; i <= count-1 ; i++){
     var trTag = createTrTag(i);
-    for(var j = 1 ; j <= count ; j++) {
-      panelCount++ ;
+    for(var j = 0 ; j <= count-1 ; j++) {
       var tdTag = createTdTag(trTag,panelCount);
-      //trTag.append(tdTag);
+      panelCount++ ;
     }
     table.append(trTag); 
   }
@@ -27,21 +32,36 @@ function creatPanel(count)
   setPanelImage();
 }
 
+/**
+*パネルと画像の紐づけを行う
+*/
 function setPanelImage()
 {
-  var random = generate_randomx(3);
+  var random = generate_randomx(16);
   clearArryObjct( panelImages ) ;
   var count = getCount() ;
-  for(var i = 0 ; i <= count ; ++i)
+  for(var i=0 ; i <= count ; ++i)
   {
-    panelImages[getPanelId(i)] = random[1];
+    var j = random[i];
+    if(j>=8)
+    {
+      j = j-8 ;
+    }
+    panelImages[getPanelId(i)] = j;
   }
 }
+
+/**
+*パネルに紐づく画像のIDを取得する
+*/
 function getPanelImageId(id)
 {
   return panelImages[getPanelId(id)];
 }
 
+/**
+*画像のソースを取得する
+*/
 function getImageSouce(id)
 {
   var path = './image/' ;
@@ -51,6 +71,9 @@ function getImageSouce(id)
   return path + name + jpg ;
 }
 
+/**
+*TRタグの生成
+*/
 function createTrTag(id)
 {
   var trTag = $('<tr>') ;
@@ -59,6 +82,9 @@ function createTrTag(id)
   return trTag ;
 }
 
+/**
+*TDタグの生成
+*/
 function createTdTag(obj,id)
 {
   var tdTag = "<td class='tdGameline' id="+getPanelId(id)+" name='panel' onclick="+'openPanel('+id+')'+"/>";
@@ -66,8 +92,48 @@ function createTdTag(obj,id)
   return tdTag ;
 }
 
+/**
+*パネルクリック時のイベント
+*/
 function openPanel(id){
-  document.getElementById(getPanelId(id)).innerHTML = "<img src="+"'"+getImageSouce(id)+"' />";
+  var imageSouce = getImageSouce(id) ;
+  var imageId = getPanelImageId(id);
+  if(clickCount==0)
+  {
+    clickCount++;
+    document.getElementById(getPanelId(id)).innerHTML = "<img class='panelImage' src="+"'"+imageSouce+"' />";
+    selectImageId=imageId;
+  }else if(clickCount==1){
+    clickCount++;
+    document.getElementById(getPanelId(id)).innerHTML = "<img class='panelImage' src="+"'"+imageSouce+"' />";
+    if(selectImageId==imageId)
+    {
+      //一致した場合もう一度
+      selectImageId=-1;
+      clickCount=0;
+      alert("正解");
+    }else{
+      //一致していない場合プレイヤー交代
+      selectImageId=-1;
+      clickCount=0;
+      changePlayer();
+    }
+  }else{
+    alert("error");
+    clickCount=0;
+  }
+}
+
+function changePlayer()
+{
+  if(nowPlayer==player1)
+  {
+    nowPlayer=player2; 
+  }else{
+    nowPlayer=player1;
+  }
+  var obj = $('#playerInfo');
+  obj.val(nowPlayer);
 }
 
 function getPanelId(id){
@@ -85,9 +151,9 @@ function changeTypeHidden(obj)
 }
 
 //オブジェクト　ゲットメソッド
-function getTdObj(id)
+function getTdObj(name)
 {
-  return $('#' + getPanelId(id));
+  return $('#' + name);
 }
 
 // オブジェクトクリアメソッド
@@ -100,6 +166,9 @@ function clearArryObjct( obj )
   }
 }
 
+/**
+*重複しない乱数配列の生成
+*/
 function generate_randomx(count) {
   //生成した乱数を格納する配列を初期化
   var generated = new Array();
@@ -120,3 +189,4 @@ function generate_randomx(count) {
   }
   return generated;  
 }
+
