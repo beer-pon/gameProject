3cf6fbc6e2a676ef6c239e0e2db1b32db53e4492
t.koudu <t.koudu@netlinks.co.jp>
2016-06-19 00:33:22 +0900

    update

--- toolers/js/fileUtiles.js
+++ toolers/js/fileUtiles.js
@@ -3,12 +3,41 @@ var fileObj ;
 $(function(){
   setFile("selfile","text1");
 });
+
+var errorMessageObj = {
+  messageField:null,
+  errorMessage:"",
+  setField:(function(id){
+    this.messageField=$('#'+id);
+    }
+  ),
+  addErrorMassage:(function(lineNumber,message){
+      if("" === this.errorMessage){
+        this.errorMessage = lineNumber + "  " + message ;
+      }else{
+        this.errorMessage = this.errorMessage +  "\n" + lineNumber + "  " + message ;
+      }
+    }
+  ),
+  printErrorMassage:(function(){
+    if(null === this.messageField){
+      alert("error");        
+    }else{
+      this.messageField.val(this.errorMessage);
+    }
+  }),
+  clearMassage:(function(){
+    this.errorMessage = "" ;
+  }),
+};
+
 // fileObj 設定
 function setFile(fileId,textAreaId){
   fileObj = document.getElementById(fileId);  
-
   //ファイルオブジェクトのイベント設定
   fileObj.addEventListener("change",function(evt){
+  errorMessageObj.setField("result");
+  errorMessageObj.clearMassage();
 
     var file = evt.target.files;
     //FileReaderの作成
@@ -20,8 +49,8 @@ function setFile(fileId,textAreaId){
     reader.onload = function(ev){
       //テキストエリアに表示する
       var fileText = reader.result;
-      checkCodeRules(fileText);
       $('#' + textAreaId).val(fileText) ;
+      checkCodeRules(fileText);
     }
 
   },false);
@@ -29,5 +58,61 @@ function setFile(fileId,textAreaId){
 }
 
 function checkCodeRules(text){
+  // error文言を格納するオブジェクト
+  text.replace('/\r\n|\r/g', "\n") ;
+  var lines = text.split( '\n' );
+
+  $.each(lines,function(i,line){
+    i++ ;
+    // ;の前の空白確認
+    if( null != line.match(/[^\s]+;/) ){
+      errorMessageObj.addErrorMassage("error  :: ",";の前の空白確認");      
+      errorMessageObj.addErrorMassage(i,line);      
+    }
+
+    // "{"より前に空白以外の文字がある行
+    if( null != line.match(/[^\s]+[\s]*{/) ){
+      errorMessageObj.addErrorMassage("error  :: ","\{より前に空白以外の文字がある");
+      errorMessageObj.addErrorMassage(i,line);      
+    }
+
+    // "}"より前に空白以外の文字がある行
+    if( null != line.match(/[^\s]+[\s]*}/) ){
+      errorMessageObj.addErrorMassage("error  :: ","\}より前に空白以外の文字がある行");
+      errorMessageObj.addErrorMassage(i,line);      
+    }
+
+    // "("の直後に空白がない行
+    if( null != line.match(/\([^\s]+/) ){
+      errorMessageObj.addErrorMassage("error  :: ","(の直後に空白がない");
+      errorMessageObj.addErrorMassage(i,line);      
+    }
+
+    // ")"の直前に空白がない行
+    if( null != line.match(/[^\s]+\)/) ){
+      errorMessageObj.addErrorMassage("error  :: ",")の直前に空白がない");
+      errorMessageObj.addErrorMassage(i,line);      
+    }
+
+    // ";"以降にコメント以外がある行 ≒ ";"以降に"/"以外で始まる文字がある行
+    if( null != line.match(/;\s*[^/\n\s]/) ){
+      errorMessageObj.addErrorMassage("error  :: ",";以降にコメント以外がある。;以降に/以外で始まる文字がある行");
+      errorMessageObj.addErrorMassage(i,line);      
+    }
+
+    // returnの値を括弧で囲んでる行
+    if( null != line.match(/return.*\(/) ){
+      errorMessageObj.addErrorMassage("error  :: ","returnの値を括弧で囲んでる");
+      errorMessageObj.addErrorMassage(i,line);      
+    }
+
+    // 宣言と同時に初期化されていないint/char/long ※配列まで考慮してない
+    if( null != line.match(/^\s*(int|char|long)(?!.*=).*$/) ){
+      errorMessageObj.addErrorMassage("error  :: ","宣言と同時に初期化されていないint/char/long ※配列まで考慮してない");
+      errorMessageObj.addErrorMassage(i,line);
+    }
+  });
+  errorMessageObj.printErrorMassage();
 }
+
 
