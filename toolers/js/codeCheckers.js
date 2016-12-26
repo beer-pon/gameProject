eb81a02659e8d418601a24e919582d3a5859750b
tatsuya kozu <bigorsmall_t@yahoo.co.jp>
2016-12-26 23:43:29 +0900

    update js

--- toolers/js/codeCheckers.js
+++ toolers/js/codeCheckers.js
@@ -4,11 +4,6 @@
     evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
   }
 
-  // Setup the dnd listeners.
-  var dropZone = document.getElementById('drop_zone');
-  dropZone.addEventListener('dragover', handleDragOver, false);
-  dropZone.addEventListener('drop', handleFileSelect, false);
-
   function handleFileSelect(evt) {
     evt.stopPropagation();
     evt.preventDefault();
@@ -16,13 +11,21 @@
     var files = evt.dataTransfer.files; // FileList object.
 
     // files is a FileList of File objects. List some properties.
-    var output = [];
-    for (var i = 0, f; f = files[i]; i++) {
-      output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
-                  f.size, ' bytes, last modified: ',
-                  f.lastModifiedDate.toLocaleDateString(), '</li>');
+    errorMessageObj.setField("result");
+    errorMessageObj.clearMassage();
+
+    //FileReaderの作成
+    var reader = new FileReader();
+    //テキスト形式で読み込む
+    reader.readAsText(files[0]);
+  
+    //読込終了後の処理
+    reader.onload = function(ev){
+      //テキストエリアに表示する
+      var fileText = reader.result;
+      $('#text1').val(fileText) ;
+      checkCodeRules(fileText);
     }
-    document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
   }
 
 
@@ -31,6 +34,12 @@
 var fileObj ;
 $(function(){
   setFile("selfile","text1");
+
+  // Setup the dnd listeners.
+  var dropZone = document.getElementById('drop_zone');
+  dropZone.addEventListener('dragover', handleDragOver, false);
+  dropZone.addEventListener('drop', handleFileSelect, false);
+
 });
 
 var errorMessageObj = {
